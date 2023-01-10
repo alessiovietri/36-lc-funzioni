dea5d6be77d80a5c4b69769f97e81de5f2acc629
Alessio Vietri <info@alessiovietri.it>
2023-01-10 11:45:27 +0100

    Completato LC 1

---
+++ 1_somma/js/scripts.js
@@ -0,0 +1,16 @@
+function sommaNumeri(num1, num2) {
+  
+    const risultato = num1 + num2; 
+
+    return risultato;
+     
+}
+
+// const somma = sommaNumeri(49, 2);
+
+const numeroUno = 49;
+let numeroDue = 2;
+const somma = sommaNumeri(numeroUno, numeroDue);
+
+console.log('SOMMA', somma); // 51
+
